82601b57a2c7ccb3dc80e04af62a102379c06997
Supul Mallikarachchi <supulm@outlook.com>
2017-04-25 00:28:36 +0530

    changed the module.exports

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'Yj3SY8Q1e7ZaD6ls0iUJkYZq8',
-  consumer_secret:      'K2g9osqUZM1KmluVtniia3Jy9xl35cRYdQRakKMtO0FIPkB8m6',
-  access_token:         '856545441176985600-evmGHyJyrKaYm534jwY3LNkGo4ggdyA',
-  access_token_secret:  'nCkhvJHeXKpyYqu59q12H7nM7KMg41bA3bLGJ4LWlrVzG',
+  consumer_key:         '/*Enter your consumer key here*/',
+  consumer_secret:      '/*Enter your consumer secret here*/',
+  access_token:         '/*Enter your access token here*/',
+  access_token_secret:  '/*Enter your access token secret here*/',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
